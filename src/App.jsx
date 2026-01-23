eb8798bbc30f3feeb38b240046836d2557766a08
student umeshtare465 <umeshtare465@gmail.com>
2026-01-24 01:07:37 +0530

    teminal forming and learninf dragging

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./app.scss";
 import Dock from "./components/Dock";
 import Nav from "./components/Nav";
+import MacWindow from "./components/windows/MacWindow";
 const App = () => {
   return (
     <main>
       <Nav />
       <Dock />
+      <MacWindow>
+        <h1>hi myname is</h1>
+      </MacWindow>
     </main>
   );
 };
